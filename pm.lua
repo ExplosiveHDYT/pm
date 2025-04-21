3fced5733521b34b505f6e04c183611d4b5cb672
ExplosiveHDYT <82344578+ExplosiveHDYT@users.noreply.github.com>
2025-04-21 10:05:02 -0400

    Update pm.lua

--- pm.lua
+++ pm.lua
@@ -2,22 +2,11 @@
 local command = ...
 local name = select(2, ...)
 
-
--- Update Package Manager
-pm = "https://example.com/pm.lua"
-
 local url = {
-  -- Various Packages
   hello = "https://example.com/hello.lua",
   test = "https://example.com/test.lua"
 }
 
 if command == "add" then
   shell.run("wget", url[name], name..".lua")
-
-
-elseif command == "update" then
-  if url[name] then
-    shell.run("wget", https://example.com/pm.lua, name..".lua")
-
-
+end
