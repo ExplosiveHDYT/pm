bda077adb8b82d61ae603ba65b41571d0f6fbf8b
ExplosiveHDYT <82344578+ExplosiveHDYT@users.noreply.github.com>
2025-04-21 10:25:38 -0400

    Update pm.lua

--- pm.lua
+++ pm.lua
@@ -4,9 +4,20 @@ local name = select(2, ...)
 
 local url = {
   hello = "https://example.com/hello.lua",
-  test = "https://example.com/test.lua"
+  smp = "https://raw.githubusercontent.com/ExplosiveHDYT/pm/refs/heads/main/smp.lua",
+  pm = "https://raw.githubusercontent.com/ExplosiveHDYT/pm/refs/heads/main/pm.lua"
 }
 
 if command == "add" then
-  shell.run("wget", url[name], name..".lua")
+  print("Warning, package" .. url[name] .. ", continue? (y/n)")
+  local answer = read()
+    if answer == "y" then
+    if fs.exists(name .. ".lua") then
+        shell.run("rm", name .. ".lua")
+    end
+    shell.run("wget", url[name], name..".lua")
+    
+    else
+    print("operation cancelled")
+    end
 end
