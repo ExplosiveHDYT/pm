737f76215e0528d4118c93f39b3cd88260e01089
ExplosiveHDYT <82344578+ExplosiveHDYT@users.noreply.github.com>
2025-04-26 18:43:02 -0400

    Update ishinstaller.lua

--- ishinstaller.lua
+++ ishinstaller.lua
@@ -1,2 +1,2 @@
-shell.run("wget", https://raw.githubusercontent.com/m-rtijn/ISH/refs/heads/master/ishd.lua, "ishd.lua")
-shell.run("wget", https://raw.githubusercontent.com/m-rtijn/ISH/refs/heads/master/ish_client.lua, "ishd_client.lua")
+shell.run("wget", "https://raw.githubusercontent.com/m-rtijn/ISH/refs/heads/master/ishd.lua", "ishd.lua")
+shell.run("wget", "https://raw.githubusercontent.com/m-rtijn/ISH/refs/heads/master/ish_client.lua", "ishd_client.lua")
