78c0a8bd5b118c22321ec53df5a4a4e13adadbe2
ExplosiveHDYT <82344578+ExplosiveHDYT@users.noreply.github.com>
2025-05-20 01:35:48 -0400

    Update format.lua

--- format.lua
+++ format.lua
@@ -1,6 +1,6 @@
-print("Delete all files? (y/n)")
+print("WARNING, DELETING ALL FILES, CONTINUE? (y/n)")
 if read() == "y" then
-  for _,f in ipairs(fs.list("")) do
-    if f ~= "rom" and f ~= "pm.lua" then fs.delete(f) end
-  end
+  for _,f in ipairs(fs.list("")) do if f~="rom" and f~="pm.lua" then fs.delete(f) end end
+else
+  print("Operation cancelled")
 end
